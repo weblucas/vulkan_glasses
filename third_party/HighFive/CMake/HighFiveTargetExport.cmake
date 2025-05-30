e31d70fa20e1e5008a69a57ca1a137b8514f4d10
Lucas Teixeira <weblucas@gmail.com>
2025-05-30 21:28:03 +0200

    add csv_processor to the instalation folder and avoid highfive to do the same

--- third_party/HighFive/CMake/HighFiveTargetExport.cmake
+++ third_party/HighFive/CMake/HighFiveTargetExport.cmake
@@ -14,35 +14,35 @@ target_link_libraries(HighFive INTERFACE libheaders libdeps)
 
 # Generate ${PROJECT_NAME}Config.cmake
 
-include(CMakePackageConfigHelpers)
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/HighFiveConfig.cmake.in
-  ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
-  INSTALL_DESTINATION share/${PROJECT_NAME}/CMake)
+# include(CMakePackageConfigHelpers)
+# configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/HighFiveConfig.cmake.in
+#   ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+#   INSTALL_DESTINATION share/${PROJECT_NAME}/CMake)
 
-write_basic_package_version_file(
-    ${PROJECT_NAME}ConfigVersion.cmake
-    VERSION ${PROJECT_VERSION}
-    COMPATIBILITY AnyNewerVersion)
+# write_basic_package_version_file(
+#     ${PROJECT_NAME}ConfigVersion.cmake
+#     VERSION ${PROJECT_VERSION}
+#     COMPATIBILITY AnyNewerVersion)
 
-install(FILES
-    CMake/HighFiveTargetDeps.cmake
-    ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
-    ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
-  DESTINATION share/${PROJECT_NAME}/CMake)
+# install(FILES
+#     CMake/HighFiveTargetDeps.cmake
+#     ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+#     ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+#   DESTINATION share/${PROJECT_NAME}/CMake)
 
 
 # Provides IMPORTED targets when using this project from build/install trees.
 
-# Specify targets to include in the HighFive Exports
-install(TARGETS HighFive libheaders libdeps
-        EXPORT HighFiveTargets)
+# # Specify targets to include in the HighFive Exports
+# install(TARGETS HighFive libheaders libdeps
+#         EXPORT HighFiveTargets)
 
-# Generate & install the Export for the INSTALL_INTERFACE
-install(EXPORT HighFiveTargets
-        NAMESPACE HighFive_
-        FILE HighFiveTargets.cmake
-        DESTINATION share/${PROJECT_NAME}/CMake)
+# # Generate & install the Export for the INSTALL_INTERFACE
+# install(EXPORT HighFiveTargets
+#         NAMESPACE HighFive_
+#         FILE HighFiveTargets.cmake
+#         DESTINATION share/${PROJECT_NAME}/CMake)
 
-# Generate the Export for the BUILD_INTERACE (hardly used)
-export(EXPORT HighFiveTargets
-       FILE "${PROJECT_BINARY_DIR}/HighFiveTargets.cmake")
+# # Generate the Export for the BUILD_INTERACE (hardly used)
+# export(EXPORT HighFiveTargets
+#        FILE "${PROJECT_BINARY_DIR}/HighFiveTargets.cmake")
