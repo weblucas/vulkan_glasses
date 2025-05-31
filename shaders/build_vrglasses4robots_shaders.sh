e34d33523c647dec2cc995e7f700c97397a0a676
Lucas Teixeira <weblucas@gmail.com>
2025-05-31 16:20:31 +0200

    add comment to the shaders compiling script

--- shaders/build_vrglasses4robots_shaders.sh
+++ shaders/build_vrglasses4robots_shaders.sh
@@ -1,3 +1,5 @@
+# only need in special cases. Cmake is already compiling this files if the option compile shaders is ON and copies the spv to the bin folder during installation. 
+# So, most user dont need to run this script.
 glslangValidator -V vrglasses4robots_shader.vert -o vrglasses4robots_shader.vert.spv 
 glslangValidator -V vrglasses4robots_shader.frag -o vrglasses4robots_shader.frag.spv 
 
